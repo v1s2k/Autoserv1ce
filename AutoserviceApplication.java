72d12ef881215c2ea88f4b96100abfb4e72d45f8
v1s2k <70753294+v1s2k@users.noreply.github.com>
2021-05-15 23:40:28 +0300

    Добавлен первый объект и его классы

--- AutoserviceApplication.java
+++ AutoserviceApplication.java
@@ -1,4 +1,4 @@
-package CourseWork.OnlineStore;
+package CourseWork.Autoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
